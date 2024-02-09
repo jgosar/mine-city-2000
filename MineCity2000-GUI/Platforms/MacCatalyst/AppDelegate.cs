5a557ab4cdde340ff4afc07201b5f07859780413
jgosar <36840705+jgosar@users.noreply.github.com>
2024-02-09 09:05:34 +0100

    Removed some unnecessary files

--- MineCity2000-GUI/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace com.mc2k.gui;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-  protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
